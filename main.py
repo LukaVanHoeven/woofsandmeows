d0999fdee970cc1918289599f2f04b3e54f3dd77
Luka van Hoeven <lukavanhoeven@gmail.com>
2026-05-27 09:43:44 +0200

    Added pytorch ligghtning

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from bcos.training import trainer
+from bcos.experiments.utils import get_configs_and_model_factory
+
 def main():
     print("Hello from woofsandmeows!")
 
+def train_bcos():
+    ## We have to see what is most convenient, calling the function like this directly or using the CLI. 
+    trainer.run_training()
 
 if __name__ == "__main__":
     main()
+    train_bcos()
