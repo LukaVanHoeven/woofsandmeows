9715d6bc328a4f222eb2f5bf70ea8ada4d4ec746
Jasper Hofman <j.d.hofman@students.uu.nl>
2026-06-01 11:53:15 +0200

    Add notebook for a basic pipeline for explaining audio fragments

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from bcos.training import trainer
-from bcos.experiments.utils import get_configs_and_model_factory
+import torch
 
-def main():
-    print("Hello from woofsandmeows!")
+# from bcos.training import trainer
+# from bcos.experiments.utils import get_configs_and_model_factory
 
-def train_bcos():
-    ## We have to see what is most convenient, calling the function like this directly or using the CLI. 
-    trainer.run_training()
+# def main():
+#     print("Hello from woofsandmeows!")
+
+# def train_bcos():
+#     ## We have to see what is most convenient, calling the function like this directly or using the CLI. 
+#     trainer.run_training()
 
 if __name__ == "__main__":
-    main()
-    train_bcos()
+    print(torch.__version__)
+    print(torch.version.hip)
+    print(torch.cuda.is_available())
+    print(torch.cuda.get_device_name(0))
